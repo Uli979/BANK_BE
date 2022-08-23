287c72b02e7e4c7773633e651550f886b5d740da
Uli979 <107367797+Uli979@users.noreply.github.com>
2022-08-23 09:28:32 -0500

    env-site_packages-django-tets

--- __init__.py
+++ __init__.py
@@ -1,53 +1,38 @@
-from .base import (
-    clear_script_prefix,
-    clear_url_caches,
-    get_script_prefix,
-    get_urlconf,
-    is_valid_path,
-    resolve,
-    reverse,
-    reverse_lazy,
-    set_script_prefix,
-    set_urlconf,
-    translate_url,
+"""Django Unit Test framework."""
+
+from django.test.client import AsyncClient, AsyncRequestFactory, Client, RequestFactory
+from django.test.testcases import (
+    LiveServerTestCase,
+    SimpleTestCase,
+    TestCase,
+    TransactionTestCase,
+    skipIfDBFeature,
+    skipUnlessAnyDBFeature,
+    skipUnlessDBFeature,
 )
-from .conf import include, path, re_path
-from .converters import register_converter
-from .exceptions import NoReverseMatch, Resolver404
-from .resolvers import (
-    LocalePrefixPattern,
-    ResolverMatch,
-    URLPattern,
-    URLResolver,
-    get_ns_resolver,
-    get_resolver,
+from django.test.utils import (
+    ignore_warnings,
+    modify_settings,
+    override_settings,
+    override_system_checks,
+    tag,
 )
-from .utils import get_callable, get_mod_func
 
 __all__ = [
-    "LocalePrefixPattern",
-    "NoReverseMatch",
-    "URLPattern",
-    "URLResolver",
-    "Resolver404",
-    "ResolverMatch",
-    "clear_script_prefix",
-    "clear_url_caches",
-    "get_callable",
-    "get_mod_func",
-    "get_ns_resolver",
-    "get_resolver",
-    "get_script_prefix",
-    "get_urlconf",
-    "include",
-    "is_valid_path",
-    "path",
-    "re_path",
-    "register_converter",
-    "resolve",
-    "reverse",
-    "reverse_lazy",
-    "set_script_prefix",
-    "set_urlconf",
-    "translate_url",
+    "AsyncClient",
+    "AsyncRequestFactory",
+    "Client",
+    "RequestFactory",
+    "TestCase",
+    "TransactionTestCase",
+    "SimpleTestCase",
+    "LiveServerTestCase",
+    "skipIfDBFeature",
+    "skipUnlessAnyDBFeature",
+    "skipUnlessDBFeature",
+    "ignore_warnings",
+    "modify_settings",
+    "override_settings",
+    "override_system_checks",
+    "tag",
 ]
